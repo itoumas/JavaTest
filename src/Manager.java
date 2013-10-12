b10d1da6e99b134eec7fd1b0f04b1bd35c1b9dd4
itoumas <itoumas@systena.co.jp>
2013-10-12 23:59:30 +0900

    一回目のコミット

---
+++ src/Manager.java
@@ -0,0 +1,11 @@
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Ningen ningen = new Ningen ("itou", 180);
+		ningen.taberu ();
+		ningen.neru ();
+	}
+
+}
---
+++ src/Ningen.java
@@ -0,0 +1,19 @@
+
+public class Ningen {
+	
+	private String name;
+	private int shincho ;
+	
+	public Ningen (String name, int shincho) {
+		this.name = name;
+		this.shincho = shincho;
+	}
+	
+	public void taberu () {
+		System.out.println ("tabeteimasu");
+	}
+	
+	public void neru () {
+		System.out.println("neteimasu");
+	}
+}
